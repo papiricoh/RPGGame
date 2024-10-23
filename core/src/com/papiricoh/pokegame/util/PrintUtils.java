41f1925b00ad7511faa7f035c4720f95afee7861
papiricoh <slendeman0200@gmail.com>
2024-10-23 10:15:25 +0200

    added PrintUtils

---
+++ core/src/com/papiricoh/pokegame/util/PrintUtils.java
@@ -0,0 +1,8 @@
+package com.papiricoh.pokegame.util;
+
+public class PrintUtils {
+
+    public static void printInfo(String msg) {
+        System.out.println(ConsoleColors.BLUE_BRIGHT + "(Info) " + ConsoleColors.RESET + msg);
+    }
+}
